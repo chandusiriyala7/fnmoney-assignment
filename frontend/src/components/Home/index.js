998c7c13c275fbce74bfba8be251038e0233d626
chandusiriyala <chandusiriyala116@gmail.com>
2024-07-23 00:03:49 +0530

    first commit

---
+++ frontend/src/components/Home/index.js
@@ -0,0 +1,20 @@
+import Header from '../Header'
+import './index.css'
+
+import {Component} from 'react'
+
+
+class Home extends Component {
+
+    render(){
+        return (
+            <div>
+                <Header />
+                
+            </div>
+        )
+    }
+}
+
+
+export default Home
